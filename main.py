93c095a2248fb8aac297276c6999e0b95d6472ce
evertone13 <102230880+evertone13@users.noreply.github.com>
2024-02-04 22:48:06 +0800

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+from langchain_openai import ChatOpenAI
+
+chat_model =ChatOpenAI()
+
+st.title('AI Prof')
+
+content=st.text_input("Type your question below")
+
+if st.button('Find Answer'):
+    with st.spinner('Finding you a solution...'):
+        result=chat_model.invoke("Please provide an answer with detailed information regarding this question : " + content)
+        st.write(result)
+
+
+
