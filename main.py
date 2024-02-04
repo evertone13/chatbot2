dd7ccc62a9a630342301f0aba9c5ec67dcb152e6
evertone13 <102230880+evertone13@users.noreply.github.com>
2024-02-04 23:35:37 +0800

    edit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # load_dotenv()
 
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+#from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 
 chat_model =ChatOpenAI()
 
